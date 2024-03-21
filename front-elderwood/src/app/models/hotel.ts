167c71e92e812fc7b0cea1d45e05840e2bdfd03b
Joseph <josephtemarioserio@gmail.com>
2024-03-20 23:45:20 -0500

    Development, v0.1.1 segundo commit en development, module, service y component para hotel.

--- front-elderwood/src/app/models/hotel.ts
+++ front-elderwood/src/app/models/hotel.ts
@@ -1,2 +1,19 @@
 export class Hotel {
+    constructor(_id="",codigo="",nombreHotel="",descripcion="",numeroContacto="",correo=""){
+        this._id =_id;
+        this.codigo = codigo;
+        this.nombreHotel = nombreHotel;
+        this.descripcion = descripcion;
+        this.numeroContacto = numeroContacto;
+        this.correo = correo;
+    }
+
+    _id:string; // This underscore _ is for MongoDB
+    codigo:string;
+    nombreHotel:string;
+    descripcion:string;
+    numeroContacto:string;
+    correo:string;
+
+
 }
--- front-elderwood/src/app/services/hotel.service.ts
+++ front-elderwood/src/app/services/hotel.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Hotel } from '../models/hotel';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelService {
 
-  constructor() { }
+selectedHotel: Hotel;
+hoteles: Hotel[];
+readonly URL_API="http://localhost:9000/api/hotel"
+
+constructor(private http: HttpClient){
+  this.selectedHotel = new Hotel;
+  this.hoteles = [];
 }
+
+//GET ALL HOTELES
+getHoteles(){
+  return this.http.get(this.URL_API);
+}
+
+//GET Hotel by id
+getHotelId(_id:string){
+  return this.http.get(this.URL_API+ `/${_id}`)
+}
+
+//POST Hotel
+PostHotel(Hotel:Hotel){
+  return this.http.post(this.URL_API,Hotel);
+}
+
+// Put Hotel
+putHotel(Hotel:Hotel){
+return this.http.put(this.URL_API+ `/${Hotel._id}`,Hotel);
+}
+
+//Delete Reserva by id
+deleteHotel(_id:string){
+  return this.http.delete(this.URL_API+ `/${_id}`)
+}
+
+} 
