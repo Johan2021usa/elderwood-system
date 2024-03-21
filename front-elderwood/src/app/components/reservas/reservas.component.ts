a704f86e7ef3e6607993c64f3cbfe0ed2e96cabf
johanLasso <lassojairo@gmail.com>
2024-03-20 23:38:42 -0500

    Johan, v0.1.0 primer commit en Johan, model, service y component ts para reserva agregado

--- front-elderwood/src/app/components/reservas/reservas.component.ts
+++ front-elderwood/src/app/components/reservas/reservas.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservaService } from '../../services/reserva.service';
 
+declare var M:any;
 @Component({
   selector: 'app-reservas',
   templateUrl: './reservas.component.html',
-  styleUrl: './reservas.component.css'
+  styleUrl: './reservas.component.css',
+  providers: [ReservaService]
 })
-export class ReservasComponent {
+export class ReservasComponent implements OnInit{
 
+  constructor(public reservaService:ReservaService){};
+
+  ngOnInit(): void {
+  }
 }
--- front-elderwood/src/app/models/reserva.ts
+++ front-elderwood/src/app/models/reserva.ts
@@ -1,2 +1,15 @@
 export class Reserva {
+    constructor(_id="",codigo="",fechaInicioReserva="",fechaFinReserva="", estadoReserva=""){
+        this._id =_id;
+        this.codigo= codigo;
+        this.fechaInicioReserva = fechaInicioReserva;
+        this.fechaFinReserva = fechaFinReserva; 
+        this.estadoReserva = estadoReserva;
+    }
+
+    _id:string;  // This underscore _ is for MongoDB
+    codigo:string;
+    fechaInicioReserva:string;
+    fechaFinReserva:string;
+    estadoReserva:string;
 }
