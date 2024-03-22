7b405f614a3bb4e1854132d771e842e9af7a3831
Joseph <josephtemarioserio@gmail.com>
2024-03-22 13:47:52 -0500

    Development, 0.1.2 tercer commit en development hotel (service,component y models)

--- front-elderwood/src/app/components/hoteles/hoteles.component.ts
+++ front-elderwood/src/app/components/hoteles/hoteles.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HotelService } from '../../services/hotel.service';
+import { NgControl, NgForm } from '@angular/forms';
+import { Hotel } from '../../models/hotel';
 
+declare var M:any;
 @Component({
   selector: 'app-hoteles',
   templateUrl: './hoteles.component.html',
-  styleUrl: './hoteles.component.css'
+  styleUrl: './hoteles.component.css',
+  providers: [HotelService]
 })
-export class HotelesComponent {
+export class HotelesComponent implements OnInit{
+
+  constructor(public HotelService:HotelService){};
+
+  ngOnInit(): void {
+    M.AutoInit();
+    /** Este componente carga el autoinit.js cuando el componente es cargado */
+  };
+
+  crearHotel(form?:NgForm){
+    this.HotelService.PostHotel(form?.value).subscribe(
+      res=>{
+        this.resetForm(form);
+        M.toast({html: "Guardado satisfactoriamente"})
+      }
+    );
+  };
+
+  resetForm(form?:NgForm){
+    if(form){
+      form.reset();
+      this.HotelService.selectedHotel = new Hotel();  
+    };
+  };
 
 }
